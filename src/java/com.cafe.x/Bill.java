19728ea413b336b5218d31ee7e4285c7dc000441
mplusf <minal.fulzele@yahoo.com>
2017-05-15 09:06:14 +0100

    Added appropriate constants for better readability

--- src/java/com.cafe.x/Bill.java
+++ src/java/com.cafe.x/Bill.java
@@ -6,13 +6,19 @@ import java.util.List;
 
 public class Bill {
 
+    private static final String TWENTY_PERCENT = "0.2";
+    private static final String TEN_PERCENT = "0.1";
+    private static final String TWENTY_POUNDS = "2000";
+    private static final int TWO_DECIMAL_POINTS = 2;
+    private static int MONEY_CONV_FACTOR = 100;
+
     public BigDecimal calculateBill(List<Menu.MenuItem> menuItems) {
 
         BigDecimal totalCost = purchaseValue(menuItems);
         return totalCost.add(getServiceCharge(menuItems));
     }
 
-    public BigDecimal purchaseValue(List<Menu.MenuItem> menuItems) {
+    private BigDecimal purchaseValue(List<Menu.MenuItem> menuItems) {
 
         return menuItems.stream().
                 map(menuItem -> menuItem.getPrice()).
@@ -20,9 +26,9 @@ public class Bill {
                 orElse(new BigDecimal("0.00"));
     }
 
-    public BigDecimal getServiceCharge(List<Menu.MenuItem> menuItems) {
-        BigDecimal serviceCharge = new BigDecimal("0.00") ;
-        for(Menu.MenuItem menuItem : menuItems) {
+    private BigDecimal getServiceCharge(List<Menu.MenuItem> menuItems) {
+        BigDecimal serviceCharge = new BigDecimal("0.00");
+        for (Menu.MenuItem menuItem : menuItems) {
             serviceCharge = serviceCharge.add(foodItemServiceCharge(menuItem));
         }
         return finalServiceCharge(serviceCharge);
@@ -32,7 +38,7 @@ public class Bill {
         /*
         Max 20 pound tip
          */
-        if(serviceCharge.compareTo(new BigDecimal("2000"))>0) {
+        if (serviceCharge.compareTo(new BigDecimal(TWENTY_POUNDS)) > 0) {
             return new BigDecimal("20.00");
         }
         return convertInPoundAndPenceFormat(serviceCharge);
@@ -43,19 +49,19 @@ public class Bill {
         For given data no need to distinguish between hot and cold food
          */
         BigDecimal itemPriceInPennies = convertInPennies(menuItem.getPrice());
-        if(menuItem.equals(Menu.MenuItem.CHEESE_SANDWICH)) {
-            return  itemPriceInPennies.multiply(new BigDecimal("0.1"));
-        } else if(menuItem.equals(Menu.MenuItem.STEAK_SANDWICH)) {
-            return itemPriceInPennies.multiply(new BigDecimal("0.2"));
+        if (menuItem.equals(Menu.MenuItem.CHEESE_SANDWICH)) {
+            return itemPriceInPennies.multiply(new BigDecimal(TEN_PERCENT));
+        } else if (menuItem.equals(Menu.MenuItem.STEAK_SANDWICH)) {
+            return itemPriceInPennies.multiply(new BigDecimal(TWENTY_PERCENT));
         }
         return new BigDecimal("0.00");
     }
 
     private BigDecimal convertInPennies(BigDecimal valueInPoundsAndPennyFormat) {
-        return valueInPoundsAndPennyFormat.multiply(BigDecimal.valueOf(100));
+        return valueInPoundsAndPennyFormat.multiply(BigDecimal.valueOf(MONEY_CONV_FACTOR));
     }
 
     private BigDecimal convertInPoundAndPenceFormat(BigDecimal valueInPennies) {
-        return valueInPennies.divide(BigDecimal.valueOf(100)).setScale(2);
+        return valueInPennies.divide(BigDecimal.valueOf(MONEY_CONV_FACTOR)).setScale(TWO_DECIMAL_POINTS);
     }
 }
--- src/java/com.cafe.x/Menu.java
+++ src/java/com.cafe.x/Menu.java
@@ -22,16 +22,5 @@ public final class Menu {
             return price;
         }
 
-        public void setPrice(BigDecimal price) {
-            this.price = price;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
     };
 }
