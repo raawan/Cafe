a170615227af9f17dba05ea98d842a14ca5727fc
mplusf <minal.fulzele@yahoo.com>
2017-05-15 07:06:59 +0100

    Step 5:   Refactored Cafe and Cafetest class to use enums for menuitems

--- src/java/com.cafe.x/Cafe.java
+++ src/java/com.cafe.x/Cafe.java
@@ -1,16 +1,34 @@
 package com.cafe.x;
 import java.math.BigDecimal;
 
+import static com.cafe.x.Cafe.MenuItem.CHEESE_SANDWICH;
+import static com.cafe.x.Cafe.MenuItem.COFFEE;
+import static com.cafe.x.Cafe.MenuItem.COLA;
+
 
 public class Cafe {
 
-    public BigDecimal purchaseValue(String menuItem) {
-        if(menuItem.equalsIgnoreCase("cola")) {
-            return new BigDecimal("0.50");
-        } else if (menuItem.equalsIgnoreCase("Cheese Sandwich")) {
-            return new BigDecimal("2.00");
-        } else {
-            return new BigDecimal("1.00");
+    public enum MenuItem {
+        COLA("cola"),
+        CHEESE_SANDWICH("Cheese Sandwich"),
+        COFFEE("Coffee");
+
+        private String name;
+        MenuItem(String name) {
+            this.name=name;
+        }
+    };
+
+    public BigDecimal purchaseValue(MenuItem menuItem) {
+        switch(menuItem) {
+            case CHEESE_SANDWICH:
+                return new BigDecimal("2.00");
+            case COFFEE:
+                return new BigDecimal("1.00");
+            case COLA:
+                return new BigDecimal("0.50");
+            default:
+                return new BigDecimal("0.00");
         }
     }
 }
--- src/test/com/cafe/x/CafeTest.java
+++ src/test/com/cafe/x/CafeTest.java
@@ -2,7 +2,7 @@ package com.cafe.x;
 
 
 import org.junit.jupiter.api.Test;
-
+import static com.cafe.x.Cafe.MenuItem.*;
 import java.math.BigDecimal;
 
 public class CafeTest {
@@ -12,21 +12,21 @@ public class CafeTest {
     public void Given_OneCola_Then_CustomerPay50p() {
 
         Cafe cafe  = new Cafe();
-        BigDecimal totalCost = cafe.purchaseValue("Cola");
+        BigDecimal totalCost = cafe.purchaseValue(COLA);
         assert totalCost.equals(new BigDecimal("0.50"));
     }
 
     @Test
     public void Given_OneCoffee_Then_CustomerPay1pound() {
         Cafe cafe  = new Cafe();
-        BigDecimal totalCost = cafe.purchaseValue("Coffee");
+        BigDecimal totalCost = cafe.purchaseValue(COFFEE);
         assert totalCost.equals(new BigDecimal("1.00"));
     }
 
     @Test
     public void Given_OneCheeseSandwich_Then_CustomerPay2pounds() {
         Cafe cafe  = new Cafe();
-        BigDecimal totalCost = cafe.purchaseValue("Cheese Sandwich");
+        BigDecimal totalCost = cafe.purchaseValue(CHEESE_SANDWICH);
         assert totalCost.equals(new BigDecimal("2.00"));
     }
 }
