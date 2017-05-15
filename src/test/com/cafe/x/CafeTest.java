c89a8dbed42182966f33a53bef7a32eab83e4beb
mplusf <minal.fulzele@yahoo.com>
2017-05-15 06:57:38 +0100

    Step 3: Another failing/red test "Given_OneCoffee_Then_CustomerPay1pound". {using Triangulation process  to narrow down the solution space}

--- src/test/com/cafe/x/CafeTest.java
+++ src/test/com/cafe/x/CafeTest.java
@@ -15,4 +15,11 @@ public class CafeTest {
         BigDecimal totalCost = cafe.purchaseValue("Cola");
         assert totalCost.equals(new BigDecimal("0.50"));
     }
+
+    @Test
+    public void Given_OneCoffee_Then_CustomerPay1pound() {
+        Cafe cafe  = new Cafe();
+        BigDecimal totalCost = cafe.purchaseValue("Coffee");
+        assert totalCost.equals(new BigDecimal("1.00"));
+    }
 }
