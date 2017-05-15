ca149b52e746c368061527cd593d218e892262db
mplusf <minal.fulzele@yahoo.com>
2017-05-15 07:01:41 +0100

    Step 5:   "Given_OneCheeseSandwich_Then_CustomerPay2pounds" is green now

--- src/java/com.cafe.x/Cafe.java
+++ src/java/com.cafe.x/Cafe.java
@@ -7,6 +7,8 @@ public class Cafe {
     public BigDecimal purchaseValue(String menuItem) {
         if(menuItem.equalsIgnoreCase("cola")) {
             return new BigDecimal("0.50");
+        } else if (menuItem.equalsIgnoreCase("Cheese Sandwich")) {
+            return new BigDecimal("2.00");
         } else {
             return new BigDecimal("1.00");
         }
